3fe74bea3f523ac19e95a45a3528b28fc321268d
Peace3B <p.keza@alustudent.com>
2024-07-03 01:53:44 -0700

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,7 +4,7 @@ Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
-
+import sys
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
@@ -17,3 +17,9 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass a subreddit as an argument.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
